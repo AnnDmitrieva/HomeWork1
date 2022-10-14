b8110a85e61fbdf8a5bf25dd065263a7bb5c90e6
Anna <anyaamdmitrieva@yandex.ru>
2022-10-14 18:30:39 +0600

    proj1

---
+++ 1/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.Write("Ведите число 1: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Ведите число 2: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+if (number1 > number2)
+{
+    Console.WriteLine($"Максимальное число {number1}");
+    Console.WriteLine($"Миниимальное число {number2}");
+}
+
+if (number1 < number2)
+{
+    Console.WriteLine($"Максимальное число {number2}");
+    Console.WriteLine($"Миниимальное число {number1}");
+}
+
+if (number1 == number2)
+{
+    Console.WriteLine("Числа равны.");
+}
