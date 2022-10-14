1604e30b2f19e389b5657936831a96a28a791654
Anna <anyaamdmitrieva@yandex.ru>
2022-10-14 21:05:20 +0600

    proj4

---
+++ 4/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int rem = 2;
+
+if (number <= 1)
+{
+    Console.WriteLine("ОШИБКА!Введите число > 1.");
+}
+
+else
+{
+    while (rem <= number)
+    {
+        Console.WriteLine($"{rem}");
+        rem = rem + 2;
+    }
+}
